91ebdd65d90463dbc7b06d6b64a725443bb40745
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-08-13 18:53:15 -0400

    Attempt to implement weather find based on location

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react';
 
-
-
 export default function App () {
   //API KEYS
   //GET WEATHER FROM SEARCHING UP
@@ -10,12 +8,46 @@ export default function App () {
   
   const [inputVal, setInputVal] = useState('');
   const [weatherVal, setWeatherVal] = useState('');
+  const [weatherAtLocationVal, setWeatherAtLocationVal] = useState('');
+  const [handledLocationRequestVal, setHandledLocationRequestVal] = useState('');
+  const [currentLatitudeVal, setLatitudeVal] = useState('');
+  const [currentLongitudeVal, setLongitudeVal] = useState('');
+
+  //Gets Current Weather of the Location You Are At
+  function getCurrentLocationWeather () {
+    navigator.geolocation.getCurrentPosition(handleLocationRequest, error);
+  }
+
+  function error (err) {
+    console.warn(`(ERROR ${err.code}): ${err.message}`);
+  }
+  
+  function handleLocationRequest(pos) {
+    console.log("Working");
+    setWeatherVal('');
+    const crds = pos.coords;
+    setLatitudeVal(`${crds.latitude}`);
+    setLongitudeVal(`${crds.longitude}`);
+    getWeather();
+    setHandledLocationRequestVal(true);
+  }
+  
   function handleClick () {
-    getWeather(inputVal);
+    setWeatherVal('');
+    if (handledLocationRequestVal) {
+      getWeather();  
+    } else {
+      getCurrentLocationWeather();
+    }
   }
 
   async function getWeather() {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}`;
+    let queryURL = "";
+    if (handledLocationRequestVal) {
+      queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}`;
+    } else {
+      queryURL = `https://api.openweathermap.org/data/3.0/onecall/overview?lon=${currentLongitudeVal}&lat=${currentLatitudeVal}&appid=${weatherAPIKey}`;
+    }
     const weatherResult = await fetchData(queryURL);
     if (weatherResult !== undefined) {
       setWeatherVal(weatherResult + " F");
@@ -30,14 +62,11 @@ export default function App () {
     <div className = "weatherHeader">
       <input type = "text" value = {inputVal} onChange={handleInputValue}/>
        <p className = "cityWeather">Weather: {weatherVal}</p>
-       <button 
-          onClick = {handleClick}
-       >
-        Click Me to Get Weather</button>
+       <button onClick = {handleClick}>Click Me to Get Weather</button>
      </div>
   );
 
-
+  //Fetches Weather Based on Provided Query
   async function fetchData (queryURL) {
     try {
       let response = await fetch(queryURL);
