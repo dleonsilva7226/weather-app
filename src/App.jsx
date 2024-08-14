84329ec78411a4cd7e0e214bf64c8dd72c3896bd
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-08-13 23:02:38 -0400

    Attempt to Fix Bug for Calling Weather Data for Current Location

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,57 @@ export default function App () {
   //GET WEATHER FROM SEARCHING UP
   const weatherAPIKey = "391cbb4a58a8cc222403d449bb858880";
   const defaultQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${weatherAPIKey}`;
-  
   const [inputVal, setInputVal] = useState('');
   const [weatherVal, setWeatherVal] = useState('');
   const [weatherAtLocationVal, setWeatherAtLocationVal] = useState('');
-  const [handledLocationRequestVal, setHandledLocationRequestVal] = useState('');
+  // const [handledLocationRequestVal, setHandledLocationRequestVal] = useState('');
   const [currentLatitudeVal, setLatitudeVal] = useState('');
   const [currentLongitudeVal, setLongitudeVal] = useState('');
 
-  //Gets Current Weather of the Location You Are At
-  function getCurrentLocationWeather () {
-    navigator.geolocation.getCurrentPosition(handleLocationRequest, error);
+  function handleClick () {
+    setWeatherVal('');
+
+    //Function that Cause Errors Below
+    // getCurrentLocationWeather(); 
+    getSearchedWeather(); 
   }
 
-  function error (err) {
+  //Gets Current Weather of the Location You Are At
+  async function error (err) {
     console.warn(`(ERROR ${err.code}): ${err.message}`);
   }
   
-  function handleLocationRequest(pos) {
+  async function success(pos) {
     console.log("Working");
-    setWeatherVal('');
     const crds = pos.coords;
     setLatitudeVal(`${crds.latitude}`);
     setLongitudeVal(`${crds.longitude}`);
-    getWeather();
-    setHandledLocationRequestVal(true);
+    console.log(`${currentLatitudeVal}, ${currentLongitudeVal}`);
   }
-  
-  function handleClick () {
-    setWeatherVal('');
-    if (handledLocationRequestVal) {
-      getWeather();  
-    } else {
-      getCurrentLocationWeather();
+
+
+  //SOMETHING IS WRONG HERE BELOW. CHECK IT OUT: Location takes a bit of time to get. Erase the try statement 
+  //Look at Brocode course and attempt to solve the issue
+  async function getCurrentLocationWeather () {
+    navigator.geolocation.getCurrentPosition(await success, await error);
+    try {
+    let queryURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${currentLatitudeVal}&lon=${currentLongitudeVal}&appid=${weatherAPIKey}`;
+
+    const weatherResult = await fetchData(queryURL);
+    if (weatherResult !== undefined) {
+      setWeatherAtLocationVal(weatherResult + " F");
+    }
+    }
+    catch (error){
+      console.log("Not Good");
     }
   }
 
-  async function getWeather() {
-    let queryURL = "";
-    if (handledLocationRequestVal) {
-      queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}`;
-    } else {
-      queryURL = `https://api.openweathermap.org/data/3.0/onecall/overview?lon=${currentLongitudeVal}&lat=${currentLatitudeVal}&appid=${weatherAPIKey}`;
-    }
+  //SOMETHING IS WRONG HERE ABOVE. CHECK IT OUT: Location takes a bit of time to get. Erase the try statement 
+  //Look at Brocode course in JavaScript for promises and api fetching and attempt to solve the issue
+
+  async function getSearchedWeather() {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${weatherAPIKey}`;
     const weatherResult = await fetchData(queryURL);
     if (weatherResult !== undefined) {
       setWeatherVal(weatherResult + " F");
@@ -61,6 +69,7 @@ export default function App () {
   return (
     <div className = "weatherHeader">
       <input type = "text" value = {inputVal} onChange={handleInputValue}/>
+       <p className = "locationWeather">Weather at Location: {weatherAtLocationVal}</p>
        <p className = "cityWeather">Weather: {weatherVal}</p>
        <button onClick = {handleClick}>Click Me to Get Weather</button>
      </div>
@@ -70,11 +79,13 @@ export default function App () {
   async function fetchData (queryURL) {
     try {
       let response = await fetch(queryURL);
+      console.log(response);
       if (!response.ok) {
         throw new Error("Invalid Place");
       }
-
       const data = await response.json();
+      console.log(data);
+      console.log();
       console.log(data.main.temp);
       let fahrenheitTemp = (data.main.temp - 273.15) * 9/5 + 32;
       return Math.round(fahrenheitTemp);
