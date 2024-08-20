a5451112a0a026c1d9fad3139550a009f294beab
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-08-20 12:54:42 -0400

    Implement Option to Search With Both City and Country

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,205 @@ export default function App () {
   const [canSearchByButton, setCanSearchByButton] = useState(true);
   const [canSearchByEnter, setCanSearchByEnter] = useState(false);
   const [weatherLocInfoArr, setWeatherLocInfoArr] = useState([]);
-  /*Make a countrycode **ARRAY OR MAP HERE** so you can 
-  search for countries by its full name or Country Code*/
+  // Country Code Map Below
+  const countryMap = new Map([
+    ["afghanistan", "AF"],
+    ["albania", "AL"],
+    ["algeria", "DZ"],
+    ["andorra", "AD"],
+    ["angola", "AO"],
+    ["antigua and barbuda", "AG"],
+    ["argentina", "AR"],
+    ["armenia", "AM"],
+    ["australia", "AU"],
+    ["austria", "AT"],
+    ["azerbaijan", "AZ"],
+    ["bahamas", "BS"],
+    ["bahrain", "BH"],
+    ["bangladesh", "BD"],
+    ["barbados", "BB"],
+    ["belarus", "BY"],
+    ["belgium", "BE"],
+    ["belize", "BZ"],
+    ["benin", "BJ"],
+    ["bhutan", "BT"],
+    ["bolivia", "BO"],
+    ["bosnia and herzegovina", "BA"],
+    ["botswana", "BW"],
+    ["brazil", "BR"],
+    ["brunei", "BN"],
+    ["bulgaria", "BG"],
+    ["burkina faso", "BF"],
+    ["burundi", "BI"],
+    ["cabo verde", "CV"],
+    ["cambodia", "KH"],
+    ["cameroon", "CM"],
+    ["canada", "CA"],
+    ["central african republic", "CF"],
+    ["chad", "TD"],
+    ["chile", "CL"],
+    ["china", "CN"],
+    ["colombia", "CO"],
+    ["comoros", "KM"],
+    ["democratic republic of the congo", "CD"],
+    ["republic of the congo", "CG"],
+    ["costa rica", "CR"],
+    ["croatia", "HR"],
+    ["cuba", "CU"],
+    ["cyprus", "CY"],
+    ["czech republic", "CZ"],
+    ["denmark", "DK"],
+    ["djibouti", "DJ"],
+    ["dominica", "DM"],
+    ["dominican republic", "DO"],
+    ["ecuador", "EC"],
+    ["egypt", "EG"],
+    ["el salvador", "SV"],
+    ["equatorial guinea", "GQ"],
+    ["eritrea", "ER"],
+    ["estonia", "EE"],
+    ["eswatini", "SZ"],
+    ["ethiopia", "ET"],
+    ["fiji", "FJ"],
+    ["finland", "FI"],
+    ["france", "FR"],
+    ["gabon", "GA"],
+    ["gambia", "GM"],
+    ["georgia", "GE"],
+    ["germany", "DE"],
+    ["ghana", "GH"],
+    ["greece", "GR"],
+    ["grenada", "GD"],
+    ["guatemala", "GT"],
+    ["guinea", "GN"],
+    ["guinea-bissau", "GW"],
+    ["guyana", "GY"],
+    ["haiti", "HT"],
+    ["honduras", "HN"],
+    ["hungary", "HU"],
+    ["iceland", "IS"],
+    ["india", "IN"],
+    ["indonesia", "ID"],
+    ["iran", "IR"],
+    ["iraq", "IQ"],
+    ["ireland", "IE"],
+    ["israel", "IL"],
+    ["italy", "IT"],
+    ["jamaica", "JM"],
+    ["japan", "JP"],
+    ["jordan", "JO"],
+    ["kazakhstan", "KZ"],
+    ["kenya", "KE"],
+    ["kiribati", "KI"],
+    ["north korea", "KP"],
+    ["south korea", "KR"],
+    ["kuwait", "KW"],
+    ["kyrgyzstan", "KG"],
+    ["laos", "LA"],
+    ["latvia", "LV"],
+    ["lebanon", "LB"],
+    ["lesotho", "LS"],
+    ["liberia", "LR"],
+    ["libya", "LY"],
+    ["liechtenstein", "LI"],
+    ["lithuania", "LT"],
+    ["luxembourg", "LU"],
+    ["madagascar", "MG"],
+    ["malawi", "MW"],
+    ["malaysia", "MY"],
+    ["maldives", "MV"],
+    ["mali", "ML"],
+    ["malta", "MT"],
+    ["marshall islands", "MH"],
+    ["mauritania", "MR"],
+    ["mauritius", "MU"],
+    ["mexico", "MX"],
+    ["micronesia", "FM"],
+    ["moldova", "MD"],
+    ["monaco", "MC"],
+    ["mongolia", "MN"],
+    ["montenegro", "ME"],
+    ["morocco", "MA"],
+    ["mozambique", "MZ"],
+    ["myanmar", "MM"],
+    ["namibia", "NA"],
+    ["nauru", "NR"],
+    ["nepal", "NP"],
+    ["netherlands", "NL"],
+    ["new zealand", "NZ"],
+    ["nicaragua", "NI"],
+    ["niger", "NE"],
+    ["nigeria", "NG"],
+    ["north macedonia", "MK"],
+    ["norway", "NO"],
+    ["oman", "OM"],
+    ["pakistan", "PK"],
+    ["palau", "PW"],
+    ["panama", "PA"],
+    ["papua new guinea", "PG"],
+    ["paraguay", "PY"],
+    ["peru", "PE"],
+    ["philippines", "PH"],
+    ["poland", "PL"],
+    ["portugal", "PT"],
+    ["qatar", "QA"],
+    ["romania", "RO"],
+    ["russia", "RU"],
+    ["rwanda", "RW"],
+    ["saint kitts and nevis", "KN"],
+    ["saint lucia", "LC"],
+    ["saint vincent and the grenadines", "VC"],
+    ["samoa", "WS"],
+    ["san marino", "SM"],
+    ["sao tome and principe", "ST"],
+    ["saudi arabia", "SA"],
+    ["senegal", "SN"],
+    ["serbia", "RS"],
+    ["seychelles", "SC"],
+    ["sierra leone", "SL"],
+    ["singapore", "SG"],
+    ["slovakia", "SK"],
+    ["slovenia", "SI"],
+    ["solomon islands", "SB"],
+    ["somalia", "SO"],
+    ["south africa", "ZA"],
+    ["south sudan", "SS"],
+    ["spain", "ES"],
+    ["sri lanka", "LK"],
+    ["sudan", "SD"],
+    ["suriname", "SR"],
+    ["sweden", "SE"],
+    ["switzerland", "CH"],
+    ["syria", "SY"],
+    ["taiwan", "TW"],
+    ["tajikistan", "TJ"],
+    ["tanzania", "TZ"],
+    ["thailand", "TH"],
+    ["timor-leste", "TL"],
+    ["togo", "TG"],
+    ["tonga", "TO"],
+    ["trinidad and tobago", "TT"],
+    ["tunisia", "TN"],
+    ["turkey", "TR"],
+    ["turkmenistan", "TM"],
+    ["tuvalu", "TV"],
+    ["uganda", "UG"],
+    ["ukraine", "UA"],
+    ["united arab emirates", "AE"],
+    ["united kingdom", "GB"],
+    ["united states", "US"],
+    ["united states of america", "US"],
+    ["uruguay", "UY"],
+    ["uzbekistan", "UZ"],
+    ["vanuatu", "VU"],
+    ["vatican city", "VA"],
+    ["venezuela", "VE"],
+    ["vietnam", "VN"],
+    ["yemen", "YE"],
+    ["zambia", "ZM"],
+    ["zimbabwe", "ZW"]
+]);
+  // Country Code Map Above
   
 
   //Searches on Every Render of the Page
@@ -68,7 +265,7 @@ export default function App () {
               <button onClick = {() => deleteComponent(weatherComponentInfo.id)} className = "deleteButton">Delete</button>
             </div>
             <div className = "weatherStatsInfo">
-              <p className = "overallWeather">Overall: {weatherComponentInfo.weatherFeeling}</p>
+              <p className = "overallWeather">{weatherComponentInfo.weatherFeeling}</p>
               <p className = "cityName">City: {weatherComponentInfo.city}</p>
               <p className = "cityWeather">Weather: {weatherComponentInfo.weather}</p>
               <p className = "countryName">Country: {weatherComponentInfo.country}</p>
@@ -94,8 +291,6 @@ export default function App () {
     //DO THIS LATER
     // getCurrentLocationWeather(); 
     //DO ABOVE LATER
-    
-    
     const weatherStats = await getSearchedWeather();
     console.log(weatherStats);
     if (weatherStats !== undefined && weatherStats.length == 7 && weatherStats[0] !== ''  && weatherStats[1] !== ''  
@@ -116,9 +311,6 @@ export default function App () {
 
     }    
   }
-
-
-
 
   //Gets Current Weather of the Location You Are At
   // async function error (err) {
@@ -181,7 +373,16 @@ export default function App () {
       infoArr[0] = infoArr[0].trim();
       if (infoArr.length <= 2) {
         infoArr[1] = infoArr[1].trim();
-        queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${infoArr[0]},${infoArr[1]}&appid=${weatherAPIKey}&units=imperial`;
+        let newInfoStr = countryMap.get(infoArr[1].toLowerCase());
+        console.log(newInfoStr);
+        if (newInfoStr !== undefined) {
+          // console.log(newInfoStr);
+          queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${infoArr[0]},${newInfoStr}&appid=${weatherAPIKey}&units=imperial`;
+        } else {
+          let countryCode = infoArr[1].toUpperCase();
+          // console.log(countryMap.get(infoArr[1]));
+          queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${infoArr[0]},${countryCode}&appid=${weatherAPIKey}&units=imperial`;
+        }
       } else {
         queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${infoArr[0]}&appid=${weatherAPIKey}&units=imperial`;
       }       
